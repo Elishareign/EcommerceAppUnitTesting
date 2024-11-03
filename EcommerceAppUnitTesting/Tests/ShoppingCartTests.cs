3e14fcf8475f76180119810acc244280b6e6e563
Elisha Reign Mier <elishamier13@gmail.com>
2024-11-03 23:51:28 +0800

    Add project files.

---
+++ EcommerceAppUnitTesting/Tests/ShoppingCartTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+
+namespace EcommerceAppUnitTesting.Tests
+{
+    [TestFixture]
+    public class ShoppingCartTests
+    {
+        [Test]
+        public void AddAndRemoveProduct()
+        {
+            // Arrange
+            var cart = new ShoppingCart();
+            var product = new Product { Name = "Test Product", Price = 100 };
+
+            // Act
+            cart.AddProduct(product);
+            var initialCount = cart.Products.Count;
+            cart.RemoveProduct(product);
+            var finalCount = cart.Products.Count;
+
+            // Assert
+            Assert.That(initialCount, Is.EqualTo(1));
+            Assert.That(finalCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateTotalPrice()
+        {
+            // Arrange
+            var cart = new ShoppingCart();
+            var product1 = new Product { Price = 100 };
+            var product2 = new Product { Price = 200 };
+            cart.AddProduct(product1);
+            cart.AddProduct(product2);
+
+            // Act
+            var total = cart.CalculateTotalPrice();
+
+            // Assert
+            Assert.That(total, Is.EqualTo(300));
+        }
+
+        [Test]
+        public void ApplyDiscount()
+        {
+            // Arrange
+            var cart = new ShoppingCart();
+            var product = new Product { Price = 100 };
+            cart.AddProduct(product);
+
+            // Act
+            var totalAfterDiscount = cart.ApplyDiscount(10); // 10% discount
+
+            // Assert
+            Assert.That(totalAfterDiscount, Is.EqualTo(90));
+        }
+    }
+}
